fff14a206df06a52581629bda9216e2d2de5f1e7
James Pearce <jamesquintanapearce@gmail.com>
2020-04-04 22:33:21 +1100

    Updates

---
+++ precache-manifest.f1b80f11b9976a11b381621ebe34886c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbd9ca35281e2892185d13df0d20cd90",
+    "url": "/employee-directory/index.html"
+  },
+  {
+    "revision": "596a539661adb49aa4c4",
+    "url": "/employee-directory/static/css/main.c63b6a64.chunk.css"
+  },
+  {
+    "revision": "6385d82fad7da814d686",
+    "url": "/employee-directory/static/js/2.efdeda72.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/employee-directory/static/js/2.efdeda72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "596a539661adb49aa4c4",
+    "url": "/employee-directory/static/js/main.9bcc3fa3.chunk.js"
+  },
+  {
+    "revision": "11377a30a1e93c848f66",
+    "url": "/employee-directory/static/js/runtime-main.28ec2055.js"
+  }
+]);
